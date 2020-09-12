251b5ff7ad94aec7ef8e8dda13878d6f5a73fc40
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2020-09-12 23:01:17 +0100

    v2, unlimited threads

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 <h1>
-    <img align="left" src="https://img.icons8.com/fluent/64/000000/discord-new-logo.png"></img>modmail.py
+    <img align="left" src="https://img.icons8.com/fluent/56/000000/discord-new-logo.png"></img>modmail.py
 </h1>
 <p>
-    Modmail discord bot written in python, designed for small servers, which inludes the following features
+    Modmail discord bot written in python, designed for any server size, which inludes the following features;
     <br>
-    - Support for three threads, meaning that there can be multiple people contact the mods at the same time
+    - Support for unlimited threads, meaning that there can be multiple people contact the mods at the same time
     <br>
     - sends all dms to a specified modmail channel where the mod can use $thread1 (reply) to send a message back using the dm thread
     <br>
@@ -15,10 +15,10 @@
     <br>
     - to send message to mods; just dm the bot account, when the message has been successfully sent, you will be notified
     <br>
-    - to reply as a mod; use $thread1 (reply), $thread2 (reply) or $thread3 (reply) to reply to the respective threads 
+    - to reply as a mod; use /thread ThreadNumber ReplyContent to reply to the respective threads 
     <br>
     <br>
     Future additions
     <br>
-    - support for unlimited threads
+    - include checks so that only the mods of a server can reply to the messages
 </p>
--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # bot token and prefix
-BOTPREFIX = "$"
+BOTPREFIX = "/"
 BOTTOKEN = ""
 MODMAILCHANNEL = 
 
@@ -16,7 +16,7 @@ client.remove_command("help")
 # changing the bot's status to "Listening to $help" and printing that the bot has logged in without any issues
 @client.event
 async def on_ready():
-    ListeningTo = discord.Activity(type=discord.ActivityType.listening, name="dms to contact mods")
+    ListeningTo = discord.Activity(type=discord.ActivityType.listening, name="to your concerns")
     await client.change_presence(status=discord.Status.online, activity=ListeningTo)
     print('We have logged in as {0.user}'.format(client))
 
@@ -30,15 +30,9 @@ async def on_message(ctx):
         ThreadNumber = int(ThreadNumberText.readline())
         ThreadNumberText.close()
         ThreadNumberText = open("./Tickets/ThreadNumber.txt", "w")
-        
-        if ThreadNumber == 3:
-            ThreadNumber = 1
-            ThreadNumberText.writelines(str(ThreadNumber))
-            ThreadNumberText.close()
-        else:
-            ThreadNumber = ThreadNumber + 1
-            ThreadNumberText.writelines(str(ThreadNumber))
-            ThreadNumberText.close()
+        ThreadNumber = ThreadNumber + 1
+        ThreadNumberText.writelines(str(ThreadNumber))
+        ThreadNumberText.close()
 
 
         # sending an embed to the modmail channel so the mods can view it
@@ -61,66 +55,61 @@ async def on_message(ctx):
         await ctx.channel.send(embed = UserEmbed)
 
         
-        Thread1Text = open("./Tickets/thread" + str(ThreadNumber) + ".txt", "w")
-        Thread1Text.writelines(str(ctx.author.id))
-        Thread1Text.close()
+        ThreadText = open("./Tickets/thread" + str(ThreadNumber) + ".txt", "w")
+        ThreadText.writelines(str(ctx.author.id))
+        ThreadText.close()
     else:
         await client.process_commands(ctx)
         return;
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send("pong")
     print("pong")
 
-@client.command(name = "thread1")
-async def thread1(ctx):
-    Thread1Text = open("./Tickets/thread1.txt")
-    Thread1 = Thread1Text.readline()
-    Thread1Text.close()
-    user = client.get_user(int(Thread1))
-    print(user)
+
+@client.command(name = "thread")
+async def thread(ctx):
+    MessageContent = ctx.message.content
+    MessageSplit = MessageContent.split(" ")
+    ThreadNumber = MessageSplit[1]
+    ThreadNumberLen = len(ThreadNumber)
+    MessageIndex = ThreadNumberLen + 8
+    ModReply = MessageContent[MessageIndex:]
+
+    ThreadText = open("./Tickets/thread" + ThreadNumber + ".txt")
+    Thread = ThreadText.readline()
+    ThreadText.close()
+    user = client.get_user(int(Thread))
+
     ReplyEmbed = discord.Embed(
-        colour = discord.Colour.light_grey()
+        colour = discord.Colour.light_gray()
     )
     ReplyEmbed.set_author(name = "ModMail", icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
     ReplyEmbed.add_field(name = "Response from:", value = ctx.author, inline = False)
-    ReplyEmbed.add_field(name = "Response content:", value = ctx.message.content[9:], inline = False)
+    ReplyEmbed.add_field(name = "Response content:", value = ModReply, inline = False)
 
-    await user.send(embed = ReplyEmbed)
-
-
-@client.command(name = "thread2")
-async def thread2(ctx):
-    Thread1Text = open("./Tickets/thread2.txt")
-    Thread1 = Thread1Text.readline()
-    Thread1Text.close()
-    user = client.get_user(int(Thread1))
-    print(user)
-    ReplyEmbed = discord.Embed(
-        colour = discord.Colour.light_grey()
+    ConfirmEmbed = discord.Embed(
+        colour = discord.Colour.light_gray()
     )
-    ReplyEmbed.set_author(name = "ModMail", icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
-    ReplyEmbed.add_field(name = "Response from:", value = ctx.author, inline = False)
-    ReplyEmbed.add_field(name = "Response content:", value = ctx.message.content[9:], inline = False)
+    ConfirmEmbed.set_author(name = "ModMail - Sent message", icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
+    ConfirmEmbed.add_field(name = "Sent to:", value = user, inline = False)
+    ConfirmEmbed.add_field(name = "Response content:", value = ModReply, inline = False)
+    ConfirmEmbed.set_footer(text = "Thread number: " + str(ThreadNumber), icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
 
     await user.send(embed = ReplyEmbed)
+    await ctx.send(embed = ConfirmEmbed)
 
-
-@client.command(name = "thread3")
-async def thread3(ctx):
-    Thread1Text = open("./Tickets/thread3.txt")
-    Thread1 = Thread1Text.readline()
-    Thread1Text.close()
-    user = client.get_user(int(Thread1))
-    print(user)
-    ReplyEmbed = discord.Embed(
-        colour = discord.Colour.light_grey()
+@client.command(name = "help")
+async def help(ctx):
+    HelpEmbed = discord.Embed(
+        colour = discord.Colour.light_gray()
     )
-    ReplyEmbed.set_author(name = "ModMail", icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
-    ReplyEmbed.add_field(name = "Response from:", value = ctx.author, inline = False)
-    ReplyEmbed.add_field(name = "Response content:", value = ctx.message.content[9:], inline = False)
+    HelpEmbed.set_author(name = "ModMail help", icon_url = "https://img.icons8.com/dusk/64/000000/mailbox-closed-flag-down.png")
+    HelpEmbed.add_field(name = "How to contact mods:", value = "To contact a moderator, please send a dm to this bot, you will recieve a reply in the same thread afterwards", inline = False)
 
-    await user.send(embed = ReplyEmbed)
+    await ctx.send(embed = HelpEmbed)
+
 
 client.run(BOTTOKEN)
